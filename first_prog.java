f8ccb2c413ebf4010457894c92f567afbb5b9ad8
pritidemo <103582175+pritidemo@users.noreply.github.com>
2022-04-13 18:56:46 +0530

    Add files via upload

---
+++ first_prog.java
@@ -0,0 +1,23 @@
+class first_prog //new class name
+{
+	public static void main(String args[]) // created a main function
+	{
+		System.out.println("Hiii this is my new lecture of java :)");
+	}
+}
+/*
+return type:
+
+
+name: Priti
+abc@123
+
+
+
+mobile
+-------
+
+public 
+protected 
+private
+*/
